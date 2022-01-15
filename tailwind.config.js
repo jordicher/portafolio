689a1d99aa67cdeb9ad18f71deb23bf1e66315b2
jordicher <jordi_sodiac@hotmail.com>
2022-01-15 23:53:25 +0100

    initial commit, default files

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors: {
+        charcoal: '#264653',
+        persian: '#2A9D8F',
+      },
+    },
+  },
+  plugins: [],
+};
